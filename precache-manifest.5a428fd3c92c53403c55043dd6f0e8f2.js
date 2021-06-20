9aa6d87b602d50fd5fc7c1e26a1a1c4ef3c4b1cc
Jan Latoch <jan.latoch@gmail.com>
2021-06-20 10:31:04 +0200

    Updates

---
+++ precache-manifest.5a428fd3c92c53403c55043dd6f0e8f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f0dcd5672be1e8c6f95f910acf9a59",
+    "url": "/pomodoroTimer/index.html"
+  },
+  {
+    "revision": "fbe26383cdfb2d3b739c",
+    "url": "/pomodoroTimer/static/css/main.53209ca4.chunk.css"
+  },
+  {
+    "revision": "8184a0e79dabd3d762e4",
+    "url": "/pomodoroTimer/static/js/2.fce14f47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pomodoroTimer/static/js/2.fce14f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe26383cdfb2d3b739c",
+    "url": "/pomodoroTimer/static/js/main.1ecda93a.chunk.js"
+  },
+  {
+    "revision": "ad6c6b8e77f044d75bfb",
+    "url": "/pomodoroTimer/static/js/runtime-main.ad03961d.js"
+  }
+]);
